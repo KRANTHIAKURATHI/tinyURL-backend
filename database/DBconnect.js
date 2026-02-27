8bf44e93f5fdb8ef62e144d09fd41174053c4d13
Harsh <Harsh@google>
2026-02-27 23:36:23 +0530

    updated

--- database/DBconnect.js
+++ database/DBconnect.js
@@ -4,23 +4,9 @@ let pool;
 
 const DBconnect = async () => {
   if (!pool) {
-    pool = mysql.createPool({
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      port: process.env.DB_PORT,
-      waitForConnections: true,
-      connectionLimit: 5,
-      queueLimit: 0,
-      ssl: {
-        rejectUnauthorized: false
-      }
-    });
-
+    pool = mysql.createPool(process.env.DB_URL);
     console.log("✅ Database pool created successfully");
   }
-
   return pool;
 };
 
